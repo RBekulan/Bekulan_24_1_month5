8ce30844c161e9d25aaf49a821f653d4d785daee
RBekulan <bekulanrysmatov4@gmail.com>
2024-03-31 16:39:18 +0600

    new

--- movie_app/admin.py
+++ movie_app/admin.py
@@ -6,11 +6,3 @@ admin.site.register(Director)
 admin.site.register(Movie)
 admin.site.register(Review)
 
-
-# from django.contrib import admin
-# from movie_app.models import *
-# # Register your models here.
-#
-# admin.site.register(Movie)
-# admin.site.register(Director)
-# admin.site.register(Review)
